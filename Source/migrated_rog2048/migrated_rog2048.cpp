20daf6bd4f5509ae887c68897ba2dcd14b631fda
Apollinaria <ApollinariaJee@users.noreply.github.com>
2021-06-03 16:50:52 +0300

    Initial commit

---
+++ Source/migrated_rog2048/migrated_rog2048.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "migrated_rog2048.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, migrated_rog2048, "migrated_rog2048" );
